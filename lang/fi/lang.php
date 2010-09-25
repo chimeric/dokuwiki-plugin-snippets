7fdb280353584495ad94e9a25e1e54d0a5cba92f
Erkki Joensuu <erkki.joensuu@gmail.com>
2010-09-25 12:04:10 +0200

    added finish translation

---
+++ lang/fi/lang.php
@@ -0,0 +1,10 @@
+﻿<?php
+/**
+ * Finnish language file for DokuWiki plugin toolbartpl
+ */
+$lang['js']['preview'] = 'Esikatsele';
+$lang['js']['insert']  = 'Lisää';
+$lang['gb_snippets']   = 'Valitse sivumalli';
+$lang['popup_title']   = 'Snippets (mallisivun valinta)';
+
+// vim:ts=4:sw=4:et:enc=utf-8:
---
+++ lang/fi/settings.php
@@ -0,0 +1,5 @@
+﻿<?php
+/**
+ * Finnish language file for DokuWiki Plugin Snippets
+ */
+$lang['snippets_page'] = 'Sivu joka sisältää mallisivulistan (täytyy sisältää järjestämätön lista mallisivuista)';
